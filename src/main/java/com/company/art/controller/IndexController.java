8a6adb6a336363d52d72e20f41c529647151d427
Kamil Blanik <kamil.lukasz.blanik@gmail.com>
2020-06-25 20:08:02 +0200

    Added home page, partial menu bar & bootstrap

---
+++ src/main/java/com/company/art/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.company.art.controller;
+
+import com.company.art.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value={"/", "/index"}, method = RequestMethod.GET)
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
